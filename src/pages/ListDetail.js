9229cc066facb03506a46df7461b77f4d7796e3a
randoelicor <Jose_Leonard_Rico@manulife.ca>
2023-01-21 20:41:05 +0800

    Mutating Context with Hooks

--- src/pages/ListDetail.js
+++ src/pages/ListDetail.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from "react";
+import { useEffect, useContext } from "react";
 import styled from "styled-components";
 import { useNavigate, useParams } from "react-router-dom";
 // import useDataFetching from "../hooks/useDataFetching";
@@ -17,19 +17,11 @@ function ListDetail() {
   let navigate = useNavigate();
   const { listId } = useParams();
 
-  // const [loading, error, data] = useDataFetching(
-  //   "https://my-json-server.typicode.com/PacktPublishing/React-Projects-Second-Edition/items/"
-  // );
-
-  const { loading, error, items: data } = useContext(ItemsContext);
-
-  const [items, setItems] = useState([]);
+  const { loading, error, items, fetchItems } = useContext(ItemsContext);
 
   useEffect(() => {
-    data &&
-      listId &&
-      setItems(data.filter((item) => item.listId === parseInt(listId)));
-  }, [data, listId]);
+    listId && !items.length && fetchItems(listId);
+  }, [fetchItems, items, listId]);
 
   return (
     <>
