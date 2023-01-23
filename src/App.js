4243ec58b2d35679b32335f196a2c5b519195ad0
randoelicor <Jose_Leonard_Rico@manulife.ca>
2023-01-23 15:31:57 +0800

    Added code splitting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
+import { Suspense, lazy } from "react";
 import styled, { createGlobalStyle } from "styled-components";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Header from "./components/Header/Header";
-import Lists from "./pages/Lists";
-import ListDetail from "./pages/ListDetail";
-import ListForm from "./pages/ListForm";
+// import Lists from "./pages/Lists";
+// import ListDetail from "./pages/ListDetail";
+// import ListForm from "./pages/ListForm";
 import AppContext from "./context/AppContext";
+
+// const Lists = lazy(() =>
+//   import(/* webpackChunkName: "Lists" */ "./pages/Lists")
+// );
+// const ListDetail = lazy(() =>
+//   import(/* webpackChunkName: "ListDetail" */ "./pages/ListDetail")
+// );
+// const ListForm = lazy(() =>
+//   import(/* webpackChunkName: "ListForm" */ "./pages/ListForm")
+// );
+
+const Lists = lazy(() => import("./pages/Lists"));
+const ListDetail = lazy(() => import("./pages/ListDetail"));
+const ListForm = lazy(() => import("./pages/ListForm"));
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -30,13 +44,15 @@ function App() {
       <AppWrapper>
         <BrowserRouter>
           <Header />
-          <AppContext>
-            <Routes>
-              <Route path="/" element={<Lists />} />
-              <Route path="/list/:listId/new" element={<ListForm />} />
-              <Route path="/list/:listId" element={<ListDetail />} />
-            </Routes>
-          </AppContext>
+          <Suspense fallback={<div>Loading...</div>}>
+            <AppContext>
+              <Routes>
+                <Route path="/" element={<Lists />} />
+                <Route path="/list/:listId/new" element={<ListForm />} />
+                <Route path="/list/:listId" element={<ListDetail />} />
+              </Routes>
+            </AppContext>
+          </Suspense>
         </BrowserRouter>
       </AppWrapper>
     </>
